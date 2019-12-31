167ccc3db75bcdf07dde845b02cfb94bf4970da9
jianghua.pu <ahuadoreen@sohu.com>
2019-12-31 15:37:12 +0800

    优化routes和login样式

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,15 +19,15 @@ const routes: Routes = [{path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent, data: {breadcrumb: '首页'
 }, children: [
   {path: 'subject', component: SubjectComponent, data: {breadcrumb: '科目'}},
-      {path: 'addSubject', component: SubjectFormComponent, data: {breadcrumb: '新增科目'}},
+      {path: 'subject/addSubject', component: SubjectFormComponent, data: {breadcrumb: '新增科目'}},
       {path: 'teacher', component: TeacherComponent, data: {breadcrumb: '教师'}},
-      {path: 'addTeacher', component: TeacherFormComponent, data: {breadcrumb: '新增教师'}},
+      {path: 'teacher/addTeacher', component: TeacherFormComponent, data: {breadcrumb: '新增教师'}},
       {path: 'gradeCourse', component: GradeCourseComponent, data: {breadcrumb: '年级课程'}},
       {path: 'class', component: ClassComponent, data: {breadcrumb: '班级'}},
-      {path: 'addClass', component: ClassFormComponent, data: {breadcrumb: '新增班级'}},
-      {path: 'courseTeacher/:id/:grade', component: CourseTeacherComponent, data: {breadcrumb: '课程及任课老师'}},
+      {path: 'class/addClass', component: ClassFormComponent, data: {breadcrumb: '新增班级'}},
+      {path: 'class/courseTeacher/:id/:grade', component: CourseTeacherComponent, data: {breadcrumb: '课程及任课老师'}},
       {path: 'student', component: StudentComponent, data: {breadcrumb: '学生'}},
-      {path: 'addStudent', component: StudentFormComponent, data: {breadcrumb: '新增学生'}},
+      {path: 'student/addStudent', component: StudentFormComponent, data: {breadcrumb: '新增学生'}},
       ], canActivate: [LoginGuard] }
 ];
 
